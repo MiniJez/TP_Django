db467183ab8da568b259dae16731450b4293eda1
Edouard CLISSON <edouardclisson@gmail.com>
2020-01-17 15:30:21 +0100

    Feat(frameworks): add Frameworks model

--- Frameworks/models.py
+++ Frameworks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Frameworks(models.Model):
+    name = models.CharField(max_length=50)
+    language = models.CharField(max_length=50)
+    using_percentage = models.PositiveIntegerField()
