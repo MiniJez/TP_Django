41cd4b86e106a97d424885443b99eaa46ed328a5
Edouard CLISSON <edouardclisson@gmail.com>
2020-01-17 16:17:12 +0100

    Feat(frameworks): reference framework model in admin

--- Frameworks/admin.py
+++ Frameworks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Frameworks
 
 # Register your models here.
+class FrameworksAdmin(admin.ModelAdmin):
+    list_display = ("name","language", "using_percentage")
+admin.site.register(Frameworks, FrameworksAdmin)
