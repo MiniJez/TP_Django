7ed92ea29aa63b1ed52c6108854011c8fa50c747
Edouard CLISSON <edouardclisson@gmail.com>
2020-02-09 12:02:38 +0100

    Feat(Views): Add create view

--- Frameworks/views.py
+++ Frameworks/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 from .models import Frameworks
 
 def index(request):
-    frameworks = Frameworks.objects.all()
+    frameworks = Frameworks.objects.order_by('using_percentage')
     template = loader.get_template('Frameworks/list.html')
     context = {"frameworks": frameworks}
+    return HttpResponse(template.render(context, request))
+
+def create(request):
+    template = loader.get_template('Frameworks/create.html')
+    context = {}
     return HttpResponse(template.render(context, request))
