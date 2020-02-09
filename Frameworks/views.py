e7540f3178d44efeab69a8c8bea14a70fdaa9b4e
Edouard CLISSON <edouardclisson@gmail.com>
2020-02-09 14:04:41 +0100

    Feat(CRUD): add update + delete

--- Frameworks/views.py
+++ Frameworks/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Frameworks
 from .forms import FrameworksForm
 
 def index(request):
-    frameworks = Frameworks.objects.order_by('using_percentage')
+    frameworks = Frameworks.objects.order_by('-using_percentage')
     template = loader.get_template('Frameworks/list.html')
     context = {"frameworks": frameworks}
     return HttpResponse(template.render(context, request))
@@ -21,4 +21,25 @@ def create(request):
 
     template = loader.get_template('Frameworks/create.html')
     context = {"form": form}
+    return HttpResponse(template.render(context, request))
+
+def delete(request, pk):
+    framework = Frameworks.objects.get(pk=pk)
+    framework.delete()
+    return HttpResponseRedirect('/')
+
+def update(request, pk):
+    if request.method == 'POST':
+        framework = Frameworks.objects.get(pk=pk)
+        form = FrameworksForm(request.POST, instance=framework)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        framework = Frameworks.objects.get(pk=pk)
+        data = {'id': framework.id, 'name': framework.name, 'language': framework.language, 'using_percentage': framework.using_percentage}
+        form = FrameworksForm(initial=data)
+
+    template = loader.get_template('Frameworks/update.html')
+    context = {"form": form, "id": framework.id}
     return HttpResponse(template.render(context, request))
